3d6c14308d66b3e01865b17b8a645114a74c7e76
Nitin Rahangdale <nitin.rahangdale@neuraflash.com>
2024-11-08 10:07:13 +0530

    Added typescript path resolvers and aliases to make path easier

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT || 8080;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("<h1>Node + Typescript Server</h1>");
+    res.send("<h1>Hello World</h1>");
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
